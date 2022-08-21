88affca211c94a7fcf0a1b15a9207c28327ec024
idanPersi <idan_perski@hotmail.com>
2022-08-22 03:18:46 +0900

    end of lesson

--- stop-watch/stop-watch.js
+++ stop-watch/stop-watch.js
@@ -1,14 +1,34 @@
 
 const stopWatchView =  document.querySelector('#stop-watch-view');
+const startStopBtn = document.querySelector('#toggle-stop-watch');
+const resetBtn = document.querySelector('#reset-stop-watch');
+const lapsView= document.querySelector('#laps-view');
+const lapsBtn = document.querySelector('#stop-watch-laps');
 
 let hundreds = 0
 let seconds = 0
 let minutes = 0
 
-function displayTime(){
-   
-    stopWatchView.innerHTML = `${minutes}:${seconds}:${hundreds}`;
+function getTimeString(){
+      let secondsString = seconds;
+   if (seconds < 10){
+    secondsString = '0'+seconds;
+   }
+   const timeString = `${minutes}:${secondsString}:${hundreds}`
 
+   return timeString
+}
+
+
+function displayTime(){
+ 
+    stopWatchView.innerHTML = `${getTimeString()}`;
+
+}
+
+function addLap(){
+    lapsView.innerHTML += `<li>${minutes}:${seconds}:${hundreds}</li>`
+    
 }
 
 
@@ -27,6 +47,34 @@ function handleTimeChange(){
 }
 
 
-setInterval(handleTimeChange,10)
+let intervalId = null;
+function toggleStopWatch() {
+  console.log('intervalId', intervalId);
+  if (intervalId == null) {
+    startWatch()
+  } else {
+    //stop the stop watch
+    stopWatch()
+  }
+}
 
-clearInterval()
+function startWatch(){
+    intervalId = setInterval(handleTimeChange, 10);
+    startStopBtn.innerHTML = 'Stop';
+}
+
+function stopWatch(){
+    clearInterval(intervalId);
+    startStopBtn.innerHTML = 'Start';
+    intervalId = null;
+}
+
+function resetStopWatch() {
+    hundreds =0
+    seconds =0
+    minutes =0
+    displayTime()
+}
+resetBtn.addEventListener('click',resetStopWatch)
+startStopBtn.addEventListener('click',toggleStopWatch);
+lapsBtn.addEventListener('click', addLap)
